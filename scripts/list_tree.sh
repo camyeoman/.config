a09396a49a0cdcb48624b0facb9de905a51e7865
Campbell Yeoman <cam@yeoman.com.au>
2021-02-09 12:44:37 +1100

    initial commit

---
+++ scripts/list_tree.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+
+if (( $(tree | wc -l) > 30 )); then
+	for i in {5..2}; do
+		if (( $(tree -L $i | wc -l) < 30 )); then
+			tree -CL $i
+			exit
+		fi
+	done
+
+	tree -CL 1
+else
+	tree -C
+fi
